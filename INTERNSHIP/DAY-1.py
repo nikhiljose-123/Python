80dbc6e7a762b4c17da6d963b7f7e2bef2efdb57
Nikhil Jose <75926880+nikhiljose-123@users.noreply.github.com>
2022-06-23 23:44:19 +0530

    Add files via upload

---
+++ INTERNSHIP/DAY-1.py
@@ -0,0 +1,11 @@
+#variables->to store a data value
+num1=1
+print("stored value is ",num1)   #print num1
+num2=2.3
+sum=num1+num2
+print(sum)
+print(type(num1))
+print(type(num2))
+num4="hello"
+print(type(num4))
+
