2172207b3cb4e27ef07a3b18f209d375f828174c
Nikhil Jose <75926880+nikhiljose-123@users.noreply.github.com>
2022-06-24 23:46:09 +0530

    Add files via upload

---
+++ INTERNSHIP/DAY-2.py
@@ -0,0 +1,60 @@
+print(10>9)
+print(10<9)
+print(10==9)
+bol=True
+print(bol)
+print(type(bol))
+name="Tom"
+print(name)
+print(type(name))
+# concatenating-adding two things
+print("My name is",name)
+# index number-printing the data individually
+print(name[0])
+print(name[1])
+print(name[2])
+# negative index we got data from back
+print(name[-1])
+print(name[-2])
+print(name[-3])
+# list
+mylist=['a',1,0.35,True]
+print(mylist)
+print(type(mylist))
+print(type(mylist[1]))
+
+
+# mutable-change
+#updating
+# append
+mylist.append("b");
+mylist.insert(1,5)
+print(mylist)
+mylist.append(1);
+print(mylist)
+mylist[0]="b","c"
+print(mylist)
+
+
+
+# deleting
+del mylist[2]
+print(mylist)
+
+
+
+
+# delete entire list
+del mylist
+print(mylist)
+
+
+
+
+
+
+
+
+
+
+
